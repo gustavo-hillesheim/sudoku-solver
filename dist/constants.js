8be90f1bfe8ba9e01b95b4fe6fffbee00974f8b8
Gustavo Hill <guhillesheim@gmail.com>
2019-11-24 13:31:56 -0300

    Removing tracking of dist/

--- dist/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SUDOKU_SIZE = 9;
-exports.GRID_SIZE = 3;
-exports.TRACK_CHANGES_X = 7;
-exports.TRACK_CHANGES_Y = 7;
-exports.TRACK_CHANGES = false;
-//# sourceMappingURL=constants.js.map
--- dist/structure/line.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const square_group_1 = require("./square-group");
-class Line extends square_group_1.SquareGroup {
-}
-exports.Line = Line;
-//# sourceMappingURL=line.js.map
